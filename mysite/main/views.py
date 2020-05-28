9310f42824f57ae53fafbbbd567ee97297e89bc4
DM1122 <34867698+DM1122@users.noreply.github.com>
2020-05-27 21:06:53 -0400

    Add users app for user registration

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,33 +1,25 @@
 from django.shortcuts import render
-
-
-posts = [
-    {
-        'author':'David',
-        'title':'A post',
-        'content':'Hellow world!',
-        'date':'august 27, 2019'
-    },
-    {
-        'author':'Bruck',
-        'title':'A post',
-        'content':'Hellow world2!',
-        'date':'Jan 22, 2029'
-    }
-
-]
+from . models import Video
 
 
 
 def home(request):
-    context = {
-        'posts':posts
-    }
+    return render(request, 'main/home.html')
 
-    return render(request, 'main/home.html', context)
 
 def about(request):
     context = {
-        'title':'About'
+        'title': 'About'
     }
+
     return render(request, 'main/about.html', context)
+
+
+def browser(request):
+    context = {
+        'title': 'Browser',
+        'videos': Video.objects.all()
+    }
+
+    return render(request, 'main/browser.html', context)
+
