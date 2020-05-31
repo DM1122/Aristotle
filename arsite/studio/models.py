d5896352eb3935f2a2ff5e4917fe641508a4fd5c
DM1122 <34867698+DM1122@users.noreply.github.com>
2020-05-31 18:46:12 -0400

    Migrate database to include MultipleChoiceQuiz model

--- arsite/studio/models.py
+++ arsite/studio/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from library.models import Video
 
-# Create your models here.
+class MultipleChoiceQuiz(models.Model):
+    video = models.ForeignKey(Video, on_delete=models.CASCADE)
+    prompt = models.TextField()
+    timestamp = models.IntegerField(default=0)
+
+    option1 = models.TextField()
+    option2 = models.TextField()
+    option3 = models.TextField()
+    option4 = models.TextField()
+    key = models.IntegerField()     # an int between 1-4
+
+    option1_redirect = models.URLField()
+    option2_redirect = models.URLField()
+    option3_redirect = models.URLField()
+    option4_redirect = models.URLField()
+
+
+    def __str__(self):
+        return f'{video}: {prompt}'
